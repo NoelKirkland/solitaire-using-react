84b9966984a47e9993c0b412b83242f63a1d2dc7
Noel Kirkland <noelkirkland@gmail.com>
2025-02-21 04:39:36 -0800

    stock pile looks good on client

--- src/solitaire/components/piles/Stock/index.jsx
+++ src/solitaire/components/piles/Stock/index.jsx
@@ -1,23 +1,19 @@
 import React, { useContext } from 'react'
 import Card from "./../../Card";
 import { filterByPile, GameStateContext } from "./../../../functions";
-const { log } = console;
+import "./styles.scss";
+// const { log } = console;
 
 
 
 const Stock = () => {
     const { gameState } = useContext(GameStateContext);
-    log('gameState in Stock:  ', gameState)
     const stockPile = filterByPile(gameState, 'stock');
-    log('stockPile in Stock:  ', stockPile)
-    log('stockPile array in Stock:  ', Object.values(stockPile))
 
   return (
     <div className='card-pile card-pile_stock'>
         {
-            Object.values(stockPile).map((cardProps, i) => {
-                // log('cardProps:  ', cardProps)
-            return <Card cardProps={cardProps} key={i} />})
+            Object.values(stockPile).map((cardProps, i) => <Card cardProps={cardProps} key={i} />)
         }
     </div>
   )
