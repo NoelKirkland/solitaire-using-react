4e72bb526abb4de70e4b64c2c42d2494154ffca7
Noel Kirkland <noelkirkland@gmail.com>
2025-02-21 20:06:51 -0800

    changed a shit load of stuff. currently broken...

--- src/solitaire/components/piles/Stock/index.jsx
+++ src/solitaire/components/piles/Stock/index.jsx
@@ -1,21 +1,22 @@
 import React, { useContext } from 'react'
+import { GameStateContext  } from './../../../../App';
 import Card from "./../../Card";
-import { filterByPile, GameStateContext } from "./../../../functions";
+import { filterByPile, /* GameStateContext, pullTopCard, handleCardClick, */ handleClicks } from "./../../../functions";
 import "./styles.scss";
-// const { log } = console;
-
-
+const { log } = console;
 
 const Stock = () => {
-    const { gameState } = useContext(GameStateContext);
+    const { gameState, setGameState } = useContext(GameStateContext);
     const stockPileCards = Object.values(filterByPile(gameState, 'stock'));
+    const handleStockPileClicks = handleClicks('stock');
+    const handleEmptyStockPileClicks = handleStockPileClicks({ location: { isEmpty: true } });
 
   return (
     <div className='card-pile card-pile_stock'>
         {stockPileCards.length > 0 ? (
-            stockPileCards.map((cardProps, i) => <Card cardProps={cardProps} key={i} />)
+            stockPileCards.map((cardProps, i) => <Card cardProps={cardProps} cardClick={handleStockPileClicks(cardProps)} key={i} />)
           ) : (
-            <div className='empty-slot'></div>
+            <div className='empty-slot' onClick={() => handleEmptyStockPileClicks(gameState, setGameState)}></div>
           )}
     </div>
   )
---
+++ src/solitaire/components/piles/Tableau/index.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useContext } from 'react'
+import Card from "./../../Card";
+import { filterByPile, GameStateContext, handleCardClick } from "./../../../functions";
+import "./styles.scss";
+const { log } = console;
+
+
+
+const Tableau = () => {
+    const { gameState, setGameState } = useContext(GameStateContext);
+    const [ tableauState, setTableauState ] = useState(filterByPile(gameState, 'tableau'))
+    const tableauPileCards = Object.values(filterByPile(gameState, 'tableau'));
+    const allSubPilesArray = tableauPileCards.reduce((allSubPiles, card) => {
+      const { subPile, pileIndex } = card.location;
+      const subPileArrayIndex = subPile - 1;
+      const subPileArray = allSubPiles[subPileArrayIndex];
+      
+      subPileArray[pileIndex] = card;
+      // subPileArray.push(card);
+
+
+      return allSubPiles;
+    }, [[], [], [], [], [], [], []]);
+    const test = handleCardClick(gameState, setGameState);
+  return (
+    <div className='game-section_tableau'>
+      <div className='card-pile tableau-pile tableau-pile_1'>
+        {allSubPilesArray[0].length > 0 ? (
+            allSubPilesArray[0].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => {
+              test(cardProps)
+              setTableauState(() => ({...tableauState}))
+            }} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_2'>
+        {allSubPilesArray[1].length > 0 ? (
+            allSubPilesArray[1].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => handleCardClick(gameState, setGameState, 'tableau', cardProps)} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_3'>
+        {allSubPilesArray[2].length > 0 ? (
+            allSubPilesArray[2].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => handleCardClick(gameState, setGameState, 'tableau', cardProps)} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_4'>
+        {allSubPilesArray[3].length > 0 ? (
+            allSubPilesArray[3].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => handleCardClick(gameState, setGameState, 'tableau', cardProps)} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_5'>
+        {allSubPilesArray[4].length > 0 ? (
+            allSubPilesArray[4].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => handleCardClick(gameState, setGameState, 'tableau', cardProps)} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_6'>
+        {allSubPilesArray[5].length > 0 ? (
+            allSubPilesArray[5].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => handleCardClick(gameState, setGameState, 'tableau', cardProps)} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+
+      <div className='card-pile tableau-pile tableau-pile_7'>
+        {allSubPilesArray[6].length > 0 ? (
+            allSubPilesArray[6].map((cardProps, i) => <Card cardProps={cardProps} cardClick={() => {
+              handleCardClick(gameState, setGameState, 'tableau', cardProps);
+              setGameState(s => ({ ...s }))
+            }} key={i} />)
+          ) : (
+            <div className='empty-slot'>
+              <p className='empty-slot-text'>♠</p>
+            </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Tableau;
+
